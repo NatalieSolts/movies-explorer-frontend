71cc1e739c1955de4abb434ce9d3e6b3a2693a13
Natalie <mrs.solts@mail.ru>
2023-06-30 00:22:52 +0500

    feat: верстка элементов

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,23 +1,27 @@
-// компонент с информацией о студенте.
+import Section from '../Section/Section';
 import './AboutMe.css';
 import imageFoto from '../../images/photo.jpg'
+import { myBirthday } from '../../utils/constants'
 import { Link } from 'react-router-dom';
-import Section from '../Section/Section';
-const AboutMe = () => {
-    return (
-    <Section title="Студент" >
-      <div className='about-me__column'>
-        <div className='about-me__info'>
-          <h3 className='about-me__name'>Наталья</h3>
-          <h4 className='about-me__profession'>Фронтенд-разработчик, 33года</h4>
-          <p className='about-me__text'>Родилась в г. Таллинн, живу сейчас в городе Пскове. У меня есть муж и прекрасная дочь.
-Свободное время посвящаю семье, спорту и творчеству. Люблю создавать игрушки в стиле тедди. 
-В сфере IT я недавно </p>
-          <Link className='about-me__link-github' to="#">Github</Link>
+import Portfolio from '../Portfolio/Portfolio'
+
+function AboutMe () {
+  return (
+    <Section title="Студент" theme='aboutme'>
+      <div className='about-me'>
+        <div className='about-me__table'>
+          <div className='about-me__info'>
+            <h3 className='about-me__name'>Наталья</h3>
+            <h4 className='about-me__profession'>Фронтенд-разработчик, 33года</h4>
+            <p className='about-me__text'>Родилась в г. Таллинн, живу сейчас в городе Пскове. У меня есть муж и прекрасная дочь.
+              Свободное время посвящаю семье, спорту и творчеству. Люблю создавать игрушки в стиле тедди.
+              В сфере IT я недавно </p>
+            <Link className='about-me__link' to="https://github.com/NatalieSolts">Github</Link>
+          </div>
+          <img className='about-me__image' src={imageFoto} alt='фотография' />
         </div>
-        <img className='about-me__photo' src={imageFoto} alt='мое фото - Наталья' />
+        <Portfolio />
       </div>
-      <h4 className='about-me__portfolio'></h4>
     </Section>
   )
 }
